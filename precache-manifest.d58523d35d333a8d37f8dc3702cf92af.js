b7468199ae8200635d271647670cd6c60313dcc3
alvaroagr <alvaro.gomez.rey@gmail.com>
2020-04-26 09:14:38 -0500

    Updates

---
+++ precache-manifest.d58523d35d333a8d37f8dc3702cf92af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3fcb324338dead68efc47bc802d2e6",
+    "url": "/react-forum-yarn/index.html"
+  },
+  {
+    "revision": "8a6d1e988763c974e56e",
+    "url": "/react-forum-yarn/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d2a8988b0d8b77e08df3",
+    "url": "/react-forum-yarn/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8a6d1e988763c974e56e",
+    "url": "/react-forum-yarn/static/js/2.e075dba2.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/react-forum-yarn/static/js/2.e075dba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a8988b0d8b77e08df3",
+    "url": "/react-forum-yarn/static/js/main.4c3997ce.chunk.js"
+  },
+  {
+    "revision": "a0bec94ddb3472be1ce2",
+    "url": "/react-forum-yarn/static/js/runtime-main.4394fcff.js"
+  }
+]);
